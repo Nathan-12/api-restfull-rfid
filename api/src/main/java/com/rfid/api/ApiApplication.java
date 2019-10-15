a19e19715f0d06bafdce321bbf8c8dbfe7a75ca9
Nathan Lima Batista <nathanlima.b@gmail.com>
2019-10-15 12:58:30 -0300

    Configuração no arquivo de inicialização para permitir que o front consuma o back

--- api/src/main/java/com/rfid/api/ApiApplication.java
+++ api/src/main/java/com/rfid/api/ApiApplication.java
@@ -2,12 +2,25 @@ package com.rfid.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("GET","POST","PUT","DELETE");
+			}
+		};
 	}
 
 }
